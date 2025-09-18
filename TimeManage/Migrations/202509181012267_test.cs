e64f24c8a1afb7d739cf3c5b8a473a1f4017751c
CubicActivity <stojmanovski355@gmail.com>
2025-09-18 13:25:50 +0200

    Version 1.1

---
+++ TimeManage/Migrations/202509181012267_test.cs
@@ -0,0 +1,18 @@
+﻿namespace TimeManage.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "PomodoroTime");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "PomodoroTime", c => c.Int(nullable: false));
+        }
+    }
+}
